072e65e352ed7f407b49c1acd04fa67cfd9fb26b
Varsha Kundarapu <123001134+Varsha2635@users.noreply.github.com>
2025-07-21 19:54:51 +0530

    Update axios.js

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://server-qxfd.onrender.com/api/v1', 
+  baseURL: 'https://server-qxfd.onrender.com', 
   withCredentials: true, 
 });
 
