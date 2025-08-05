c40f2a11bca26db91bd694ef4569e2dfd1f4e685
Varsha Kundarapu <123001134+Varsha2635@users.noreply.github.com>
2025-08-05 14:03:20 +0530

    Update axios.js

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -2,9 +2,10 @@ import axios from 'axios';
 
 const instance = axios.create({
   // baseURL: 'https://server-y78j.onrender.com', 
-  baseURL: 'https://server-production-40a3.up.railway.app',
+  baseURL: 'https://server-production-40a3.up.railway.app/',
   withCredentials: true, 
 });
 
 export default instance;
+
 
